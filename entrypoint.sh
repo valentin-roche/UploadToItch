06b33fc1cfcdaa472339320675edc45bb57e315d
Valentin Roche <35733063+valentin-roche@users.noreply.github.com>
2021-11-07 02:47:21 -0500

    Cleanup login system

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@
 # -L follows redirects
 # -O specifies output name
 curl -L -o butler.zip https://broth.itch.ovh/butler/linux-amd64/LATEST/archive/default
-unzip butler.zip
+unzip -o butler.zip
 # GNU unzip tends to not set the executable bit even though it's set in the .zip
 chmod +x butler
 # just a sanity check run (and also helpful in case you're sharing CI logs)
@@ -14,10 +14,7 @@ chmod +x butler
 chmod +x ~/bin/butler
 PATH=$PATH:~/opt/bin
 
-# Logging into butler
-butler login $BUTLER_API_KEY
+# Logging into butler using the env variable BUTLER_API_KEY, use the wharf api key
+butler login
 
 echo "Upload artifact $BUILD_NAME:$VERSION_NUMBER  from $BUILD_PATH"
-
-# Wrap it all up with logging out
-butler logout
